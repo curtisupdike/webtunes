5df1b1cffec2a4a37b7588459daa9eb4c8f16b1f
Curtis Updike <cjupdike@icloud.com>
2019-08-13 17:47:25 -0400

    Basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Router, Link } from '@reach/router';
 import useAuthorization from './hooks/useAuthorization';
 import LoginButton from './components/LoginButton/LoginButton';
+import Browse from './routes/Browse/Browse';
+import NotFound from './routes/NotFound/NotFound';
 import {
   app,
   sidebar,
   header,
   title,
+  navigation,
   player,
   main
 } from './App.module.css';
@@ -20,9 +24,18 @@ function App() {
           <h1 className={title}>webTunes</h1>
           <LoginButton isAuthorized={isAuthorized} />
         </header>
+        <nav className={navigation}>
+          <Link to="browse">Browse</Link>
+        </nav>
       </div>
       <div className={player} />
-      <main className={main} />
+      <main className={main}>
+        <Router>
+          <Browse path="/" />
+          <Browse path="browse" />
+          <NotFound default />
+        </Router>
+      </main>
     </div>
   );
 }
