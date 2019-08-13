e80e3a9ac14ebda8824df47b7e076b1969565a9c
Curtis Updike <cjupdike@icloud.com>
2019-08-13 17:14:28 -0400

    Basic layout styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import useAuthorization from './hooks/useAuthorization';
 import LoginButton from './components/LoginButton/LoginButton';
+import {
+  app,
+  sidebar,
+  header,
+  title,
+  player,
+  main
+} from './App.module.css';
 
 function App() {
   const isAuthorized = useAuthorization();
 
   return (
-    <div className="App">
-      <LoginButton isAuthorized={isAuthorized} />
+    <div className={app}>
+      <div className={sidebar}>
+        <header className={header}>
+          <h1 className={title}>webTunes</h1>
+          <LoginButton isAuthorized={isAuthorized} />
+        </header>
+      </div>
+      <div className={player} />
+      <main className={main} />
     </div>
   );
 }
