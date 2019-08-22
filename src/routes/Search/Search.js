d384982958252a1c0cc330dcc597e8595ef37b8f
Curtis Updike <cjupdike@icloud.com>
2019-08-22 13:23:38 -0400

    Search for artists

--- src/routes/Search/Search.js
+++ src/routes/Search/Search.js
@@ -2,6 +2,7 @@ import React, {Fragment, useState, useEffect } from 'react';
 import { music } from '../../services/music';
 import styles from './Search.module.css';
 import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
+import ArtistCollection from './ArtistCollection/ArtistCollection';
 import AlbumCollection from '../../components/AlbumCollection/AlbumCollection';
 import PlaylistCollection from '../../components/PlaylistCollection/PlaylistCollection';
 import NotFound from '../../components/NotFound/NotFound';
@@ -12,12 +13,15 @@ function Search({query}) {
   useEffect(() => {
     music.api.search(query, {limit: 16}).then(res =>{
       setSearch(res);
+      console.log(res);
     }).catch(err => console.log(err));
   }, [query]);
 
   return search ? (
     <Fragment>
       <h1 className={styles.title}>Search</h1>
+      <h2 className={styles.heading}>Artists</h2>
+      {search.artists ? <ArtistCollection data={search.artists.data} /> : <NotFound />}
       <h2 className={styles.heading}>Album</h2>
       { search.albums ? <AlbumCollection data={search.albums.data} /> : <NotFound /> }
       <h2 className={styles.heading}>Playlists</h2>
