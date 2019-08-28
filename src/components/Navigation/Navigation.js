456fb9a3c7459d6641291b391d395ce976774309
Curtis Updike <cjupdike@icloud.com>
2019-08-28 17:04:41 -0400

    Add links to user playlists

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,35 +1,39 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import { music } from '../../services/music';
 import PropTypes from 'prop-types';
 import { Link } from '@reach/router';
-import {
-  label,
-  active,
-  forYou,
-  browse,
-  recentlyAdded,
-  artists,
-  albums,
-  songs,
-  playlist
-} from './Navigation.module.css';
+import styles from './Navigation.module.css';
 
 function Navigation({isAuthorized}) {
+  const [playlists, setPlaylists] = useState(null);
+  useEffect(() => {
+    if (isAuthorized){
+      music.api.library.playlists().then(res => setPlaylists(res));
+    } else {
+      setPlaylists(null);
+    }
+  }, [isAuthorized]);
+
   return (
     <nav>
-      <p className={label}>Apple Music</p>
+      <p className={styles.label}>Apple Music</p>
       { isAuthorized && 
-        <NavLink to="foryou" className={forYou}>For You</NavLink> 
+        <NavLink to="foryou" className={styles.forYou}>For You</NavLink> 
       }
-      <NavLink to="browse" className={browse}>Browse</NavLink>
+      <NavLink to="browse" className={styles.browse}>Browse</NavLink>
       { isAuthorized &&
         <Fragment>
-          <p className={label}>Library</p>
-          <NavLink to="library/recent" className={recentlyAdded}>Recently Added</NavLink>
-          <NavLink to="library/albums" className={albums}>Albums</NavLink>
-          <NavLink to="artists" className={artists}>Artists</NavLink>
-          <NavLink to="songs" className={songs}>Songs</NavLink>
-          <p className={label}>Playlists</p>
-          <NavLink to="playlist" className={playlist}>Playlist</NavLink>
+          <p className={styles.label}>Library</p>
+          <NavLink to="library/recent" className={styles.recent}>Recently Added</NavLink>
+          <NavLink to="library/albums" className={styles.albums}>Albums</NavLink>
+          <NavLink to="artists" className={styles.artists}>Artists</NavLink>
+          <NavLink to="songs" className={styles.songs}>Songs</NavLink>
+          <p className={styles.label}>Playlists</p>
+          {isAuthorized && playlists && playlists.map((item, key) => (
+            <NavLink to={`library/playlist/${item.id}`} className={styles.playlist} key={key}>
+              {item.attributes.name}
+            </NavLink>
+          ))}
         </Fragment>
       }
     </nav>
@@ -43,7 +47,7 @@ function NavLink(props) {
 function isActive({className}) {
   return function(props) {
     return props.isCurrent 
-      ? { className: className + ' ' + active} 
+      ? { className: className + ' ' + styles.active} 
       : { className: className };
   } 
 }  
