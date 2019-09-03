6dd3e31629147dda912fdc0a2986014d85ad21b5
Curtis Updike <cjupdike@icloud.com>
2019-09-03 19:53:59 -0400

    Library songs route

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Browse from './routes/Browse/Browse';
 import RecentlyAdded from './routes/RecentlyAdded/RecentlyAdded';
 import LibraryAlbums from './routes/LibararyAlbums/LibraryAlbums';
 import LibraryArtists from './routes/LibraryArtists/LibraryArtists';
+import LibrarySongs from './routes/LibrarySongs/LibrarySongs';
 import LibraryAlbum from './routes/LibraryAlbum/LibraryAlbum';
 import LibraryPlaylist from './routes/LibraryPlaylist/LibraryPlaylist';
 import Search from './routes/Search/Search';
@@ -49,6 +50,7 @@ function App() {
           {isAuthorized && <RecentlyAdded path="library/recent" />}
           {isAuthorized && <LibraryAlbums path="library/albums" />}
           {isAuthorized && <LibraryArtists path="library/artists" />}
+          {isAuthorized && <LibrarySongs path="library/songs" />}
           {isAuthorized && <LibraryAlbum path="library/album/:id" />}
           {isAuthorized && <LibraryPlaylist path="library/playlist/:id" />}
           <NotFound default />
---
+++ src/routes/LibrarySongs/LibrarySongs.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { music } from '../../services/music';
+import styles from './LibrarySongs.module.css';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
+import PlaylistTracklist from '../../components/PlaylistTracklist/PlaylistTracklist';
+
+function LibrarySongs() {
+  const [songs, setSongs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    music.api.library.songs(null, {
+      limit: 100,
+      offset: songs.length
+    })
+      .then(res => {res.length === 0
+        ? setLoading(false)
+        : setSongs([...songs, ...res]);
+      })
+      .catch(e => console.error(e));
+  }, [songs]);
+
+  return songs.length > 0 ? (
+    <div className={styles.songs}>
+      <h1 className={styles.title}>Songs</h1>
+      <PlaylistTracklist tracks={songs} />
+      {loading && <LoadingSpinner />}
+    </div>
+  ) : (
+    <LoadingSpinner />
+  )
+}
+
+export default LibrarySongs;
