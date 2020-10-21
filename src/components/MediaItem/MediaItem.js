84c80d1eb31845edf13de6487573cc044402db4d
Curtis Updike <curtis.updike@icloud.com>
2020-10-21 12:37:16 -0400

    Add link to media item

--- src/components/MediaItem/MediaItem.js
+++ src/components/MediaItem/MediaItem.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import formatArtworkURL from '../../utils/formatArtworkURL';
 import styles from './MediaItem.module.scss';
-
+import { Link } from '@reach/router';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 
 function MediaCollectionItem({ attributes, size }) {
 	let music = window.MusicKit.getInstance();
-	let { artwork, name, playParams } = attributes;
-	playParams = { [playParams.kind]: playParams.id };
+	let {
+		artwork,
+		name,
+		playParams: {
+			kind, id
+		}
+	} = attributes;
+	let playParams = { [kind]: id };
 
 	function play() {
 		music
 			.setQueue(playParams)
 			.then(() => music.player.play())
-			.catch((error) => console.error(error));
+			.catch(console.error.bind(console));
 	}
 
 	return (
 		<div className={styles.mediaItem}>
 			<img src={formatArtworkURL(artwork, size)} alt={`Artwork for ${name}`} />
 			<div>
+				<Link to={`/${kind}/${id}`}></Link>
 				<button onClick={play}>
 					<Icon icon={['far', 'play-circle']} size="3x" />
 				</button>
